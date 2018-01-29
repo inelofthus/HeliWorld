fc4719342ea49804810e9f8406fa152d65724063
inelofthus <inelofthus@gmail.com>
2018-01-29 14:36:28 +0100

    assets files commited

--- core/src/com/mygdx/game/HelicopterTasks.java
+++ core/src/com/mygdx/game/HelicopterTasks.java
@@ -1,13 +1,6 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.mygdx.game.states.GameStateManager;
-import com.mygdx.game.states.MenuState;
+import 
 
 public class HelicopterTasks extends ApplicationAdapter {
 	public static final int WIDTH = 480;
